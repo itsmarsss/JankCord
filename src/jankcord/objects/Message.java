d83209f6e8c6a9a12b717e66e75f15bd6b1129ca
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-20 13:32:31 -0400

    Add getters and setters

--- src/jankcord/objects/Message.java
+++ src/jankcord/objects/Message.java
@@ -10,4 +10,28 @@ public class Message {
         this.content = content;
         this.timestamp = timestamp;
     }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
