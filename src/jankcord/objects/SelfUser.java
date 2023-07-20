1e13a89aedeaf4a7354a198969d1e63ea2d658ea
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-20 16:47:23 -0400

    Add api end point host

--- src/jankcord/objects/SelfUser.java
+++ src/jankcord/objects/SelfUser.java
@@ -2,11 +2,13 @@ package jankcord.objects;
 
 public class SelfUser extends User {
     private String password;
+    private String endPointHost;
 
-    public SelfUser(long id, String username, String avatarURL, String password) {
+    public SelfUser(long id, String username, String avatarURL, String password, String endPointHost) {
         super(id, username, avatarURL);
 
         this.password = password;
+        this.endPointHost = endPointHost;
     }
 
     public String getPassword() {
@@ -15,5 +17,13 @@ public class SelfUser extends User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEndPointHost() {
+        return endPointHost;
+    }
+
+    public void setEndPointHost(String endPointHost) {
+        this.endPointHost = endPointHost;
     }
 }
