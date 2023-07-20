67e2ba1383c27599f6214e7ff4a690fbdbe07c5d
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-20 18:18:17 -0400

    Add constructor for admin use

--- src/jankcord/objects/FullUser.java
+++ src/jankcord/objects/FullUser.java
@@ -2,15 +2,23 @@ package jankcord.objects;
 
 import java.util.Base64;
 
-public class SelfUser extends User {
+public class FullUser extends User {
     private String password;
     private String endPointHost;
+    private String status;
 
-    public SelfUser(long id, String username, String avatarURL, String password, String endPointHost) {
+    public FullUser(long id, String username, String avatarURL, String password, String endPointHost) {
         super(id, username, avatarURL);
 
         this.password = password;
         this.endPointHost = decode(endPointHost) + "/api/v1/";
+    }
+
+    public FullUser(long id, String username, String avatarURL, String password, String endPointHost, String status) {
+        super(id, username, avatarURL);
+
+        this.password = password;
+        this.status = status;
     }
 
     private String decode(String endPointHost) {
@@ -34,5 +42,13 @@ public class SelfUser extends User {
 
     public void setEndPointHost(String endPointHost) {
         this.endPointHost = endPointHost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
