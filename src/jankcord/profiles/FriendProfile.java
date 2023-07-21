118d332901d9f092b0eafe8d2c4fee4d2af086b1
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-21 11:34:10 -0400

    Fix formatting

--- src/jankcord/profiles/FriendProfile.java
+++ src/jankcord/profiles/FriendProfile.java
@@ -10,32 +10,32 @@ import javax.swing.JPanel;
 import jankcord.newclasses.ResourceLoader;
 
 public class FriendProfile extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	public FriendProfile() {
-		// Init
-		setLayout(null);
-		setBackground(new Color(47, 49, 54));
-		setPreferredSize(new Dimension(448, 92));
-		
-		// Icon
-		JLabel channelIcon = new JLabel();
-		channelIcon.setSize(72, 72);
-		channelIcon.setLocation(12, 8);
-		channelIcon.setIcon(ResourceLoader.loader.getFriendProfileIcon());
-		
-		add(channelIcon);
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-		// Username
-		JLabel usernameLabel = new JLabel("Friends");
-		usernameLabel.setSize(328, 40);
-		usernameLabel.setLocation(100, 26);
-		usernameLabel.setForeground(new Color(142, 146, 151));
-		usernameLabel.setFont(new Font("Whitney", Font.PLAIN, 28));
-		
-		add(usernameLabel);
-	}
+    public FriendProfile() {
+        // Init
+        setLayout(null);
+        setBackground(new Color(47, 49, 54));
+        setPreferredSize(new Dimension(448, 92));
+
+        // Icon
+        JLabel channelIcon = new JLabel();
+        channelIcon.setSize(72, 72);
+        channelIcon.setLocation(12, 8);
+        channelIcon.setIcon(ResourceLoader.loader.getFriendProfileIcon());
+
+        add(channelIcon);
+
+        // Username
+        JLabel usernameLabel = new JLabel("Friends");
+        usernameLabel.setSize(328, 40);
+        usernameLabel.setLocation(100, 26);
+        usernameLabel.setForeground(new Color(142, 146, 151));
+        usernameLabel.setFont(new Font("Whitney", Font.PLAIN, 28));
+
+        add(usernameLabel);
+    }
 }
