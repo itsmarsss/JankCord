293287321dfde549642e44eae44a115cd8f7d304
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-21 11:09:10 -0400

    Create login interface

---
+++ src/jankcord/JankLogin.java
@@ -0,0 +1,233 @@
+package jankcord;
+
+import jankcord.components.WindowButtonListener;
+import jankcord.components.WindowButtons;
+import jankcord.objects.FullUser;
+import jankcord_admin.JankcordAdmin;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class JankLogin extends JFrame {
+    private JLabel usernameLabel = new JLabel("Username:");
+    private JTextField usernameInput = new JTextField();
+    private JLabel passwordLabel = new JLabel("Password:");
+    private JPasswordField passwordInput = new JPasswordField();
+    private JLabel serverLabel = new JLabel("Server:");
+    private JTextField serverInput = new JTextField();
+    private JButton loginButton = new JButton("Login");
+    private JLabel statusLabel = new JLabel("");
+
+    // Frame dragging
+    private int posX = 0, posY = 0;
+    private boolean drag = false;
+
+    public JankLogin() {
+        List<Image> icons = new ArrayList<>();
+        icons.add(new ImageIcon("src/resources/Icon1.png").getImage());
+        icons.add(new ImageIcon("src/resources/Icon2.png").getImage());
+        icons.add(new ImageIcon("src/resources/Icon3.png").getImage());
+        icons.add(new ImageIcon("src/resources/Icon4.png").getImage());
+        setIconImages(icons);
+
+        // Frame Init
+        setUndecorated(true);
+        getContentPane().setLayout(null);
+        setResizable(true);
+        getContentPane().setBackground(new Color(32, 34, 37));
+        Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(500, 750);
+        setLocation((int) screenDim.getWidth() / 2 - getWidth() / 2, (int) screenDim.getHeight() / 2 - getHeight() / 2);
+
+        getContentPane().addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                drag = true;
+                posX = e.getX();
+                posY = e.getY();
+            }
+        });
+        getContentPane().addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                if (drag) {
+                    setLocation(e.getXOnScreen() - posX, e.getYOnScreen() - posY);
+                }
+            }
+        });
+
+
+        Font windowButtonFont = new Font(null, Font.PLAIN, 28);
+        JLabel closeButton = new JLabel("✕", SwingConstants.CENTER);
+
+        closeButton.setOpaque(true);
+        closeButton.setSize(62, 50);
+        closeButton.setFont(windowButtonFont);
+        closeButton.setLocation(getWidth() - closeButton.getWidth(), 0);
+        closeButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setVisible(false);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                e.getComponent().setBackground(new Color(237, 66, 69));
+                e.getComponent().setForeground(new Color(255, 255, 255));
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                e.getComponent().setBackground(new Color(237, 66, 69));
+                e.getComponent().setForeground(new Color(255, 255, 255));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                e.getComponent().setBackground(new Color(32, 34, 37));
+                e.getComponent().setForeground(new Color(185, 187, 190));
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+        });
+        closeButton.setBackground(new Color(32, 34, 37));
+        closeButton.setForeground(new Color(159, 161, 165));
+
+        getContentPane().add(closeButton);
+
+        usernameLabel.setSize(150, 30);
+        usernameLabel.setLocation(100, 100);
+        usernameLabel.setForeground(new Color(114, 118, 125));
+        usernameLabel.setFont(new Font("Whitney", Font.BOLD, 28));
+        getContentPane().add(usernameLabel);
+
+        usernameInput.setSize(300, 45);
+        usernameInput.setLocation(100, 150);
+        usernameInput.setBackground(new Color(56, 58, 64));
+        usernameInput.setForeground(new Color(219, 222, 225));
+        usernameInput.setCaretColor(Color.white);
+        usernameInput.setFont(new Font("Whitney", Font.BOLD, 28));
+        usernameInput.setBorder(null);
+        getContentPane().add(usernameInput);
+
+        passwordLabel.setSize(150, 30);
+        passwordLabel.setLocation(100, 250);
+        passwordLabel.setForeground(new Color(114, 118, 125));
+        passwordLabel.setFont(new Font("Whitney", Font.BOLD, 28));
+        getContentPane().add(passwordLabel);
+
+        passwordInput.setSize(300, 45);
+        passwordInput.setLocation(100, 300);
+        passwordInput.setBackground(new Color(56, 58, 64));
+        passwordInput.setForeground(new Color(219, 222, 225));
+        passwordInput.setCaretColor(Color.white);
+        passwordInput.setFont(new Font("Whitney", Font.BOLD, 28));
+        passwordInput.setBorder(null);
+        getContentPane().add(passwordInput);
+
+        serverLabel.setSize(150, 30);
+        serverLabel.setLocation(100, 400);
+        serverLabel.setForeground(new Color(114, 118, 125));
+        serverLabel.setFont(new Font("Whitney", Font.BOLD, 28));
+        getContentPane().add(serverLabel);
+
+        serverInput.setSize(300, 45);
+        serverInput.setLocation(100, 450);
+        serverInput.setBackground(new Color(56, 58, 64));
+        serverInput.setForeground(new Color(219, 222, 225));
+        serverInput.setCaretColor(Color.white);
+        serverInput.setFont(new Font("Whitney", Font.BOLD, 28));
+        serverInput.setBorder(null);
+        getContentPane().add(serverInput);
+
+        statusLabel.setSize(300, 30);
+        statusLabel.setLocation(100, 550);
+        statusLabel.setForeground(new Color(237, 66, 69));
+        statusLabel.setFont(new Font("Whitney", Font.BOLD, 20));
+        getContentPane().add(statusLabel);
+
+        loginButton.setSize(300, 50);
+        loginButton.setLocation(100, 600);
+        loginButton.setFont(new Font("Whitney", Font.BOLD, 28));
+        loginButton.setBorder(null);
+        loginButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                HashMap<String, String> headers = new HashMap<>();
+
+                String username = usernameInput.getText();
+                String password = new String(passwordInput.getPassword());
+                String server = serverInput.getText();
+
+                headers.put("username", username);
+                headers.put("password", password);
+
+                String response = ServerCommunicator.sendHttpRequest(server + "/api/v1/login", headers);
+
+                System.out.println(response);
+
+                if (response == null) {
+                    statusLabel.setText("Error contacting server.");
+
+                    return;
+                }
+
+                if (response.equals("403")) {
+                    System.out.println("Credentials incorrect; exiting program");
+                    statusLabel.setText("Incorrect credentials.");
+                }
+
+                long id = 0;
+                String avatarURL = null;
+
+                try {
+                    // Parse the JSON string
+                    JSONParser parser = new JSONParser();
+                    JSONObject jsonObject = (JSONObject) parser.parse(response);
+
+                    // Read values from each message object
+                    id = (Long) jsonObject.get("id");
+                    avatarURL = (String) jsonObject.get("avatarURL");
+
+                    Jankcord.setFullUser(new FullUser(id, username, avatarURL, password, server + "/api/v1/"));
+
+                    dispose();
+
+                    new Jankcord();
+                } catch (Exception ex) {
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                e.getComponent().setBackground(new Color(128, 132, 142));
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                e.getComponent().setBackground(new Color(109, 111, 120));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                e.getComponent().setBackground(new Color(78, 80, 88));
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+        });
+        loginButton.setBackground(new Color(78, 80, 88));
+        loginButton.setForeground(new Color(219, 222, 225));
+
+        getContentPane().add(loginButton);
+    }
+}
