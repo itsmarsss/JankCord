cbbc572f2fedb7c6cfd2f98288cd6ab9a0c2a530
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-20 20:58:42 -0400

    Add baseplate

--- src/jankcord_admin/apihandlers/GetFriends.java
+++ src/jankcord_admin/apihandlers/GetFriends.java
@@ -2,12 +2,28 @@ package jankcord_admin.apihandlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import jankcord_admin.JankcordAdmin;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class GetFriends implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        if (!JankcordAdmin.authorized(exchange)) {
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write("403".getBytes());
+            outputStream.close();
 
+            return;
+        }
+
+        String friends = """
+                {
+                    friends: [
+                        %s
+                    ]
+                }        
+                """;
     }
 }
