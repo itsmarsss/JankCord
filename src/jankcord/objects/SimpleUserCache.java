24ec05f95d025600fe2e55301a5d03d51e5143da
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-21 22:36:44 -0400

    Add cached resized image

--- src/jankcord/objects/SimpleUserCache.java
+++ src/jankcord/objects/SimpleUserCache.java
@@ -1,20 +1,35 @@
 package jankcord.objects;
 
+import jankcord.newclasses.ResourceLoader;
+
 import java.awt.*;
 
 public class SimpleUserCache {
     private String username;
     private String avatarURL;
-    private Image avatar;
+    private Image avatar72;
+    private Image avatar80;
+    private static Image temp72 = ResourceLoader.loader.getTempProfileIcon().getImage().getScaledInstance(72, 72, Image.SCALE_FAST);
+    private static Image temp80 = ResourceLoader.loader.getTempProfileIcon().getImage().getScaledInstance(80, 80, Image.SCALE_FAST);
 
     public SimpleUserCache(String username, String avatarURL, Image avatar) {
         this.username = username;
         this.avatarURL = avatarURL;
-        this.avatar = avatar;
+        this.avatar72 = avatar.getScaledInstance(72, 72, Image.SCALE_FAST);
+        this.avatar80 = avatar.getScaledInstance(80, 80, Image.SCALE_FAST);
     }
 
-    public SimpleUserCache(Image avatar) {
-        this.avatar = avatar;
+    public SimpleUserCache() {
+        this.avatar72 = null;
+        this.avatar80 = null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getAvatarURL() {
@@ -25,19 +40,25 @@ public class SimpleUserCache {
         this.avatarURL = avatarURL;
     }
 
-    public Image getAvatar() {
-        return avatar;
+    public Image getAvatar72() {
+        if (avatar72 == null) {
+            return temp72;
+        }
+        return avatar72;
     }
 
-    public void setAvatar(Image avatar) {
-        this.avatar = avatar;
+    public void setAvatar72(Image avatar72) {
+        this.avatar72 = avatar72;
     }
 
-    public String getUsername() {
-        return username;
+    public Image getAvatar80() {
+        if (avatar80 == null) {
+            return temp80;
+        }
+        return avatar80;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setAvatar80(Image avatar80) {
+        this.avatar80 = avatar80;
     }
 }
