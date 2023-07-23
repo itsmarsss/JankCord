4eec9c6d1a1960cadbe5e62ce1bd2a9468347f1d
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-23 16:12:10 -0400

    Update instance fields

--- src/jankcord/objects/GroupChat.java
+++ src/jankcord/objects/GroupChat.java
@@ -4,20 +4,30 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GroupChat {
-    private long id;
+    private String id;
+    private ArrayList<User> members;
     private ArrayList<Message> messages;
 
-    public GroupChat(long id, ArrayList<Message> messages) {
+    public GroupChat(String id, ArrayList<User> members, ArrayList<Message> messages) {
         this.id = id;
+        this.members = members;
         this.messages = messages;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
+    }
+
+    public ArrayList<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(ArrayList<User> members) {
+        this.members = members;
     }
 
     public ArrayList<Message> getMessages() {
