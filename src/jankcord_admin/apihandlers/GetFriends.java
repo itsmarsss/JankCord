d8e154c9f8ebc9c534ebe2264942646bcfe18211
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-23 16:11:38 -0400

    Update getfriends to support group chats

--- src/jankcord_admin/apihandlers/GetFriends.java
+++ src/jankcord_admin/apihandlers/GetFriends.java
@@ -3,11 +3,16 @@ package jankcord_admin.apihandlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import jankcord.objects.FullUser;
+import jankcord.objects.GroupChat;
+import jankcord.objects.User;
 import jankcord.tools.ServerCommunicator;
 import jankcord_admin.AdminDataBase;
 import jankcord_admin.JankcordAdmin;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class GetFriends implements HttpHandler {
     @Override
@@ -31,13 +36,63 @@ public class GetFriends implements HttpHandler {
                     """.formatted(account.getId(), account.getUsername(), account.getAvatarURL()));
         }
 
+        Map<String, List<String>> requestHeaders = exchange.getRequestHeaders();
+
+        String username = requestHeaders.get("username").get(0);
+
+        long currentID = -1;
+        for (FullUser account : AdminDataBase.getAccounts()) {
+            if (account.getUsername().equals(username)) {
+                currentID = account.getId();
+            }
+        }
+
+        if (currentID == -1) {
+            ServerCommunicator.sendResponse(exchange, "403");
+            return;
+        }
+
+        StringBuilder groupChatsList = new StringBuilder();
+        for (Map.Entry<String, GroupChat> entry : AdminDataBase.getGroupChats().entrySet()) {
+            boolean save = false;
+
+            StringBuilder groupChatMembers = new StringBuilder();
+
+            for (User member : entry.getValue().getMembers()) {
+                if (member.getId() == currentID) {
+                    save = true;
+                }
+                groupChatMembers.append("""
+                        {
+                            "id": %s,
+                            "username": "%s",
+                            "avatarURL": "%s"
+                        },
+                        """.formatted(member.getId(), member.getUsername(), member.getAvatarURL()));
+            }
+
+            if (save) {
+                groupChatsList.append("""
+                        {
+                            "chatID": "%s",
+                            "members": [
+                                %s
+                            ]
+                        },
+                        """.formatted(entry.getValue().getId(), groupChatMembers.toString()));
+            }
+
+        }
         String friends = """
                 {
                     "friends": [
                         %s
+                    ],
+                    "groupChats": [
+                        %s
                     ]
                 }
-                """.formatted(friendsList.toString());
+                """.formatted(friendsList.toString(), groupChatsList.toString());
 
         ServerCommunicator.sendResponse(exchange, friends);
     }
