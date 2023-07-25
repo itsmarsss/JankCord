37e40c376f8dd9a4700273e22bf97d8f2f92197c
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 02:57:04 -0400

    Document Jank ChannelProfile

--- src/jankcord/profiles/ChannelProfile.java
+++ src/jankcord/profiles/ChannelProfile.java
@@ -15,91 +15,128 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+// ChannelProfile, child of JPanel; channel listing
 public class ChannelProfile extends JPanel {
+    // Instance fields
     private User friend;
     private JLabel usernameLabel;
 
+    // Constructor, parameter friend
     public ChannelProfile(User friend) {
+        // Set field
+        this.friend = friend;
+
+        // Get avatar of friend or default to default pfp
         Image avatar = Jankcord.avatarCache.getOrDefault(friend.getId(), new SimpleUserCache()).getAvatar72();
 
-        // Init
+        // Set JPanel Properties
         setLayout(null);
         setBackground(null);
         setPreferredSize(new Dimension(420, 92));
 
         // Icon
         JLabel channelIcon = new JLabel();
+
+        // Icon Init
         channelIcon.setSize(72, 72);
         channelIcon.setLocation(12, 8);
         channelIcon.setIcon(new ImageIcon(avatar));
 
+        // Add Icon
         add(channelIcon);
+
 
         // Username
         usernameLabel = new JLabel(friend.getUsername());
+
+        // Username Init
         usernameLabel.setSize(328, 40);
         usernameLabel.setLocation(100, 23);
         usernameLabel.setForeground(new Color(142, 146, 151));
         usernameLabel.setFont(new Font("Whitney", Font.PLAIN, 28));
 
+        // Add username
         add(usernameLabel);
 
+
+        // Add mouse listener to listing
         addMouseListener(new MouseListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-            }
+            public void mouseClicked(MouseEvent e) {}
 
+            // Mouse pressed
             @Override
             public void mousePressed(MouseEvent e) {
+                // Set colors
                 setBackground(new Color(59, 60, 66));
                 usernameLabel.setForeground(new Color(255, 255, 255));
             }
 
+            // Mouse released
             @Override
             public void mouseReleased(MouseEvent e) {
+                // Check if new text place
                 if (!Jankcord.getOtherID().equals(friend.getId() + "")) {
+                    // Set text place
                     Jankcord.setOtherID(friend.getId() + "");
+
+                    // Inform new text place
                     Jankcord.setNewOtherID(true);
                     Jankcord.setInServer(false);
                     Jankcord.setInServerCheck(true);
+
+                    // Reset messages
                     Jankcord.getChatBoxArea().resetMessages();
+
+                    // Query for new messages
                     Jankcord.queryForNewMessages();
+
+                    // Reset channel list display
                     Jankcord.getChannelList().resetDisplays();
 
+                    // Set channel name
                     Jankcord.getChatBoxArea().setChannelName(friend.getUsername());
 
+                    // Update colors
                     setBackground(new Color(59, 60, 66));
                     usernameLabel.setForeground(new Color(255, 255, 255));
-
-                    // System.out.println("Viewing: " + friend.getUsername() + " [" + friend.getId() + "]");
                 }
             }
 
+            // Mouse enter
             @Override
             public void mouseEntered(MouseEvent e) {
+                // If text place is not current
                 if (!Jankcord.getOtherID().equals(friend.getId() + "")) {
+                    // Update colors
                     setBackground(new Color(54, 55, 61));
                     usernameLabel.setForeground(new Color(219, 222, 225));
                 }
             }
 
+            // Mouse exit
             @Override
             public void mouseExited(MouseEvent e) {
+                // If text place is not current
                 if (!Jankcord.getOtherID().equals(friend.getId() + "")) {
+                    // Update colors
                     setBackground(null);
                     usernameLabel.setForeground(new Color(142, 146, 151));
                 }
             }
         });
-
-        this.friend = friend;
     }
 
+    /**
+     * Reset display colors
+     */
     public void resetDisplay() {
+        // Reset colors
         setBackground(null);
         usernameLabel.setForeground(new Color(142, 146, 151));
     }
 
+    // Getters and setters
     public User getFriend() {
         return friend;
     }
