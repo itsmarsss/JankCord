5aa147c543739259ad94fd2b8354383ccc76d63f
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 02:30:55 -0400

    Document Jank GroupChat

--- src/jankcord/objects/GroupChat.java
+++ src/jankcord/objects/GroupChat.java
@@ -3,14 +3,18 @@ package jankcord.objects;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+// GroupChat, contains all group chat information
 public class GroupChat {
+    // Instance fields
     private String id;
     private String chatName;
     private String chatIconURL;
     private ArrayList<Long> members;
     private ArrayList<Message> messages;
 
+    // Constructor to set all values
     public GroupChat(String id, String chatName, String chatIconURL, ArrayList<Long> members, ArrayList<Message> messages) {
+        // Set all fields
         this.id = id;
         this.chatName = chatName;
         this.chatIconURL = chatIconURL;
@@ -18,20 +22,33 @@ public class GroupChat {
         this.messages = messages;
     }
 
+    // Alternate constructor, set only id, members, and messages
     public GroupChat(String id, ArrayList<Long> members, ArrayList<Message> messages) {
+        // Set all fields
         this.id = id;
-        this.chatName = "Unnamed Group Chat";
+        this.chatName = "Unnamed Group Chat"; // Placeholder, cannot be empty
         this.members = members;
         this.messages = messages;
     }
 
+    // Alternate constructor, set only id, chatname, and chaticon
     public GroupChat(String id, String chatName, String chatIconURL) {
+        // Set all fields
         this.id = id;
         this.chatName = chatName;
         this.chatIconURL = chatIconURL;
     }
 
+    /**
+     * Checks if two GroupChat objects are the same
+     *
+     * @param tempGroupChat GroupChat to be compared with
+     * @return true/false depending on similarity
+     */
     public boolean isEqual(GroupChat tempGroupChat) {
+        // Check if each field is equal
+        // If not, return false
+
         if(!id.equals(tempGroupChat.getId())) {
             return false;
         }
@@ -41,6 +58,7 @@ public class GroupChat {
         return chatIconURL.equals(tempGroupChat.getChatIconURL());
     }
 
+    // Getters and setters
     public String getId() {
         return id;
     }
