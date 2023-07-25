9da1b839d8047e90e5f1c58c3605212a4ba1ca7b
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-24 23:33:41 -0400

    Document Jank labels

--- src/jankcord/components/label/JankLabel.java
+++ src/jankcord/components/label/JankLabel.java
@@ -3,10 +3,14 @@ package jankcord.components.label;
 import javax.swing.*;
 import java.awt.*;
 
-public class JankLabel extends Label {
+// JankCord's label, child of JLabel
+public class JankLabel extends JLabel {
+    // Constructor to set text
     public JankLabel(String text) {
+        // Super; set text
         super(text);
 
+        // Set Jlabel properties
         setSize(250, 30);
         setLocation(18, 10);
         setForeground(new Color(114, 118, 125));
