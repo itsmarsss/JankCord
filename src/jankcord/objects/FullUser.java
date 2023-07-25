ae7cb088baa71db85c859efe9eca7b0d43f65e63
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 02:26:34 -0400

    Document Jank FullUser

--- src/jankcord/objects/FullUser.java
+++ src/jankcord/objects/FullUser.java
@@ -1,24 +1,34 @@
 package jankcord.objects;
 
+// FullUser, contains all user information, child of User
 public class FullUser extends User {
+    // Instance fields, built upon user: password, endPointHost, and status
     private String password;
     private String endPointHost;
     private String status;
 
+    // Constructor, set everything but status
     public FullUser(long id, String username, String avatarURL, String password, String endPointHost) {
+        // Super; set id, username, and avatarURL
         super(id, username, avatarURL);
 
+        // Set password and endPointHost
         this.password = password;
         this.endPointHost = endPointHost;
     }
 
+    // Altername constructor, set everything
     public FullUser(long id, String username, String avatarURL, String password, String endPointHost, String status) {
+        // Super; set id, username, and avatarURL
         super(id, username, avatarURL);
 
+        // Set remaining values
+        this.endPointHost = endPointHost;
         this.password = password;
         this.status = status;
     }
 
+    // Getters and setters
     public String getPassword() {
         return password;
     }
