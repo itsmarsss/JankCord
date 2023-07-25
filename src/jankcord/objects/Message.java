b2ccab9697e950503c288fb3f0a24feb67d07fa7
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 02:33:09 -0400

    Document Jank Message

--- src/jankcord/objects/Message.java
+++ src/jankcord/objects/Message.java
@@ -1,16 +1,26 @@
 package jankcord.objects;
 
+// Message, contains all message information
 public class Message {
+    // Instance fields
     private long senderID;
     private String content;
     private long timestamp;
 
+    // Constructor, set all fields
     public Message(long senderID, String content, long timestamp) {
+        // Set all fields
         this.senderID = senderID;
         this.content = content;
         this.timestamp = timestamp;
     }
 
+    /**
+     * Checks if two Message objects are the same
+     *
+     * @param tempMessage Message to be compared with
+     * @return true/false depending on similarity
+     */
     public boolean isEqual(Message tempMessage) {
         if(!content.equals(tempMessage.getContent())) {
             return false;
@@ -18,6 +28,7 @@ public class Message {
         return timestamp == tempMessage.getTimestamp();
     }
 
+    // Getters and setters
     public long getSenderID() {
         return senderID;
     }
