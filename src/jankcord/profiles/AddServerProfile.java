ed1de48f341caefbad65778e0b86dffb6fd64012
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 02:51:07 -0400

    Document Jank AddServerProfile

--- src/jankcord/profiles/AddServerProfile.java
+++ src/jankcord/profiles/AddServerProfile.java
@@ -4,6 +4,7 @@ import javax.swing.JLabel;
 
 import jankcord.tools.ResourceLoader;
 
+// AddServerProfile, child of JLabel; this class is for show; returns an image; this is not mandatory
 public class AddServerProfile extends JLabel {
 	public AddServerProfile() {
 		setIcon(ResourceLoader.loader.getAddProfileIcon());
