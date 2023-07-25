b72224e614be3e43543fd09a59ff7cca2c2d4464
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 02:43:34 -0400

    Document Jank User

--- src/jankcord/objects/User.java
+++ src/jankcord/objects/User.java
@@ -1,17 +1,30 @@
 package jankcord.objects;
 
+// User, contains all relevant user information to a typical user
 public class User {
+    // Instance fields
     private long id;
     private String username;
     private String avatarURL;
 
+    // Constructor to set all fields
     public User(long id, String username, String avatarURL) {
+        // Set all fields
         this.id = id;
         this.username = username;
         this.avatarURL = avatarURL;
     }
 
+    /**
+     * Checks if two User objects are the same
+     *
+     * @param tempFriend to compare to
+     * @return true/false depending on similarity
+     */
     public boolean isEqual(User tempFriend) {
+        // Check if each field is equal
+        // If not, return false
+
         if (id != tempFriend.id) {
             return false;
         }
@@ -21,11 +34,17 @@ public class User {
         return avatarURL.equals(tempFriend.getAvatarURL());
     }
 
+    /**
+     * Override toString method to only return username
+     *
+     * @return username
+     */
     @Override
     public String toString() {
         return username;
     }
 
+    // Getters and setters
     public long getId() {
         return id;
     }
