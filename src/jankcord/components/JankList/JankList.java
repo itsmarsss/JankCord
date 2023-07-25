eb86d1b39cb1c92dece53167e4ee1a72a67d788a
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-24 23:32:02 -0400

    Document Jank lists

--- src/jankcord/components/JankList/JankList.java
+++ src/jankcord/components/JankList/JankList.java
@@ -10,12 +10,17 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+// JankCord's list, child of JList
 public class JankList extends JList {
+    // Store a version of the previous list before an action
     private int[] prev;
 
+    // Constructor, with list of objects, label to update, and update template
     public JankList(DefaultListModel<?> list, JLabel updateLabel, String template) {
+        // Super; set list object
         super(list);
 
+        // Set Jlist properties
         setBackground(new Color(43, 45, 49));
         setForeground(new Color(219, 222, 225));
         setFont(new Font("Whitney", Font.BOLD, 30));
@@ -24,28 +29,37 @@ public class JankList extends JList {
         setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         setBorder(new EmptyBorder(10, 10, 10, 10));
 
+        // Add mouse listener; action when list item is being selected
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+                // Check if shift is down
                 if (e.isShiftDown()) {
+                    // Shift based multiselect is unsupported
+                    // Undo what user did
                     setSelectedIndices(prev);
                 }
 
-                if (getSelectedIndices().length <= 20) {
-                    updateLabel.setText(String.format(template, (20 - getSelectedIndices().length)));
+                // Check if 20 members has been reached including current user, max group chat size is 20
+                if (getSelectedIndices().length <= 19) {
+                    // Update label, inform user of available slots
+                    updateLabel.setText(String.format(template, (19 - getSelectedIndices().length)));
+                    // Update previous selection values
                     prev = getSelectedIndices();
-                } else {
+                } else { // Otherwise
+                    // Undo what user did
                     setSelectedIndices(prev);
                 }
-
             }
         });
     }
 
+    // Getter for prev
     public int[] getPrev() {
         return prev;
     }
 
+    // Setter for prev
     public void setPrev(int[] prev) {
         this.prev = prev;
     }
