468ca00e50660b62a8b90952a50e42ac8aef3998
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 12:05:32 -0400

    Document GetGroupMessages

--- src/jankcord_admin/apihandlers/GetGroupMessages.java
+++ src/jankcord_admin/apihandlers/GetGroupMessages.java
@@ -16,18 +16,21 @@ import java.util.Map;
 public class GetGroupMessages implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        //System.out.println("Group Messages Requested");
-
+        // Check if user is authorized
         if (!JankcordAdmin.authorized(exchange)) {
+            // If not authorized, return 403 response code
             ServerCommunicator.sendResponse(exchange, "403");
+            // Return
             return;
         }
 
+        // Get header
         Map<String, List<String>> requestHeaders = exchange.getRequestHeaders();
 
         String chatID = requestHeaders.get("otherID").get(0);
 
-        String message = """
+        // Message template
+        final String MESSAGE = """
                 {
                     "id": %s,
                     "content": "%s",
@@ -35,27 +38,37 @@ public class GetGroupMessages implements HttpHandler {
                 },
                 """;
 
-        StringBuilder users = new StringBuilder("");
+        // Users and messages builder
+        StringBuilder users = new StringBuilder();
         StringBuilder messages = new StringBuilder();
 
+        // If chat not stored in database
         if (!AdminDataBase.getGroupChats().containsKey(chatID)) {
+            // Create File object
             File file = new File(AdminDataBase.getParent() + "/groupmessages/" + chatID + ".json");
+
+            // If file exists
             if (file.exists()) {
+                // Read file
                 JankFileKit.readGroupMessages(chatID + ".json");
-            } else {
+            } else { // Otherwise
+                // Create it
                 JankFileKit.create(file);
             }
-        } else {
+        } else { // If stored
+            // Loop each member and append to list
             for (long member : AdminDataBase.getGroupChats().get(chatID).getMembers()) {
                 users.append(member).append(",");
             }
 
+            // Loop each message and format then append to list
             for (Message msg : AdminDataBase.getGroupChats().get(chatID).getMessages()) {
-                messages.append(message.formatted(msg.getSenderID(), msg.getContent(), msg.getTimestamp()));
+                messages.append(MESSAGE.formatted(msg.getSenderID(), msg.getContent(), msg.getTimestamp()));
             }
         }
 
-        String textJSON = """
+        // Main group messages JSON string and format it with friends builder
+        String groupmessages = """
                 {
                     "users": [%s],
                     "messages": [
@@ -67,12 +80,16 @@ public class GetGroupMessages implements HttpHandler {
                 }
                 """.formatted(users.toString(), messages.toString(), chatID, AdminDataBase.getGroupChats().get(chatID).getChatName(), AdminDataBase.getGroupChats().get(chatID).getChatIconURL());
 
-        JankFileKit.writeFile(AdminDataBase.getParent() + "/groupmessages/" + chatID + ".json", textJSON);
+        // Write group messages
+        JankFileKit.writeFile(AdminDataBase.getParent() + "/groupmessages/" + chatID + ".json", groupmessages);
 
+        // If database doesn't contain this group chat
         if (!AdminDataBase.getGroupChats().containsKey(chatID)) {
+            // Read it
             JankFileKit.readGroupMessages(chatID + ".json");
         }
 
-        ServerCommunicator.sendResponse(exchange, textJSON);
+        // Return group messages to client
+        ServerCommunicator.sendResponse(exchange, groupmessages);
     }
 }
