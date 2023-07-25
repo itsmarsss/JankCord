fcd4a21404b58960f4cfed59862e6a0fb47d485e
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 11:59:18 -0400

    Document GetFriends

--- src/jankcord_admin/apihandlers/GetFriends.java
+++ src/jankcord_admin/apihandlers/GetFriends.java
@@ -14,16 +14,20 @@ import java.util.Map;
 public class GetFriends implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        //System.out.println("Friends Requested");
-
+        // Check if user is authorized
         if (!JankcordAdmin.authorized(exchange)) {
+            // If not authorized, return 403 response code
             ServerCommunicator.sendResponse(exchange, "403");
+            // Return
             return;
         }
 
+        // Friends builder
         StringBuilder friendsList = new StringBuilder();
 
+        // Loop through all accounts
         for (FullUser account : AdminDataBase.getAccounts()) {
+            // Append JSON formatted friend including id, username, and avatarURL
             friendsList.append("""
                     {
                         "id": %s,
@@ -33,22 +37,32 @@ public class GetFriends implements HttpHandler {
                     """.formatted(account.getId(), account.getUsername(), account.getAvatarURL()));
         }
 
+        // Get header
         Map<String, List<String>> requestHeaders = exchange.getRequestHeaders();
 
         String username = requestHeaders.get("username").get(0);
 
+        // Find current account ID, set to -1
         long currentID = -1;
+
+        // Loop through all account
         for (FullUser account : AdminDataBase.getAccounts()) {
+            // If username matches
             if (account.getUsername().equals(username)) {
+                // Update current ID
                 currentID = account.getId();
             }
         }
 
+        // If account ID is -1 (unchanged)
         if (currentID == -1) {
+            // Return 409 response code
             ServerCommunicator.sendResponse(exchange, "403");
+            // Return
             return;
         }
 
+        // Main friends JSON string and format it with friends builder
         String friends = """
                 {
                     "friends": [
@@ -57,6 +71,7 @@ public class GetFriends implements HttpHandler {
                 }
                 """.formatted(friendsList.toString());
 
+        // Return friends to client
         ServerCommunicator.sendResponse(exchange, friends);
     }
 }
