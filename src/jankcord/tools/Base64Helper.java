07c1a1a65cd0671aa0ef02384a1eb9de0bbf3e0b
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 03:20:59 -0400

    Document Jank Base64Helper

--- src/jankcord/tools/Base64Helper.java
+++ src/jankcord/tools/Base64Helper.java
@@ -2,19 +2,30 @@ package jankcord.tools;
 
 import java.util.Base64;
 
+// Base 64 related helper
 public class Base64Helper {
+    /**
+     * Encodes a decoded string
+     *
+     * @param decodedString string to encode
+     * @return string encoded version of decodedString
+     */
     public static String encode(String decodedString) {
-        // encode
-        String encodedString = Base64.getEncoder().encodeToString(decodedString.getBytes());
-
-        return encodedString;
+        // Encode and return
+        return Base64.getEncoder().encodeToString(decodedString.getBytes());
     }
 
+    /**
+     * Decodes an encoded string
+     *
+     * @param encodedString string to decode
+     * @return string decoded version of encodedString
+     */
     public static String decode(String encodedString) {
-        // decode
+        // Decode
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
-        String decodedString = new String(decodedBytes);
 
-        return decodedString;
+        // Return decoded
+        return new String(decodedBytes);
     }
 }
