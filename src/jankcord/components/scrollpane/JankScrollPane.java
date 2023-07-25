bdfaf864b4ffba8389418b07e7f995fcc1c34fcd
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 00:12:18 -0400

    Document Jank scrollpanes

--- src/jankcord/components/scrollpane/JankScrollPane.java
+++ src/jankcord/components/scrollpane/JankScrollPane.java
@@ -8,112 +8,155 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseWheelEvent;
 
+// JankCord's scroll panes, child of JScrollPane
 public class JankScrollPane extends JScrollPane {
-
+    // Instance variables for JankScrollPane
+    // Used for smooth scrolling
     private int destinationY;
-    private Timer timer;
+    private Timer smoothScrollTimer;
     private int multiplier;
     private int counter;
     private int prevMax;
 
+    // Used for smooth scrolling to bottom
+    private Timer scrollBottomTimer;
+    private int totalSteps = 25; // Total steps to reach the bottom in 0.5 seconds
+
+    // Constructor, sets scroll pane size and location and child component
     public JankScrollPane(int width, int height, int x, int y, JComponent child) {
+        // Super; set child
         super(child);
 
+        // Set JScrollPane properties
         setOpaque(true);
         setBorder(null);
         setLocation(x, y);
         setSize(width, height);
-        setBackground(new Color(54, 57, 63));
         getVerticalScrollBar().setUnitIncrement(0);
         getHorizontalScrollBar().setUnitIncrement(15);
+        setBackground(new Color(54, 57, 63));
         getVerticalScrollBar().setBackground(getBackground());
         getVerticalScrollBar().setPreferredSize(new Dimension(15, 0));
         getHorizontalScrollBar().setPreferredSize(new Dimension(0, 0));
         getVerticalScrollBar().setUI(new JankScrollBar(new Color(46, 51, 56), new Color(32, 34, 37), true));
         getHorizontalScrollBar().setUI(new JankScrollBar(new Color(46, 51, 56), new Color(32, 34, 37), false));
 
+        // Assign smooth scrolling variables
         multiplier = 175;
         prevMax = -1;
 
-        timer = new Timer(10, e -> {
+        // Assign timer with 10 ms delay
+        smoothScrollTimer = new Timer(10, (e) -> {
+            // Check if counter has reached max, prevent infinite looping
             if (counter >= 40) {
+                // Reset counter
                 counter = 0;
-                timer.stop();
+                // Stop timer
+                smoothScrollTimer.stop();
             }
 
+            // Increment timer
             counter++;
 
+            // Make necessary calculations; current y scroll, needed y scroll, current step value
             int currentY = getViewport().getViewPosition().y;
             int diffY = destinationY - currentY;
             int step = Math.max(5, Math.abs(diffY) / 10);
 
-            // System.out.println(destinationY + " " + Math.abs(diffY) + " " + step);
+            // Check if destination arrived
             if (Math.abs(diffY) <= step) {
+                // Set current scroll to destination
                 getVerticalScrollBar().setValue(destinationY);
+                // Reset counter
                 counter = 0;
-                timer.stop();
-            } else {
+                // Stop timer
+                smoothScrollTimer.stop();
+            } else { // Otherwise
+                // Calculate new y value
                 int newY = currentY + (diffY > 0 ? step : -step);
+                // Set current scroll to new y value
                 getVerticalScrollBar().setValue(newY);
+                // Keep timer running
             }
         });
 
         // Smooth scroll on mouse wheel events
-        addMouseWheelListener(e -> {
-            smoothScroll(e);
-        });
+        addMouseWheelListener(this::smoothScroll);
 
+        // Scroll to bottom time
         double scrollTimeInSeconds = 0.5;
-        scrollTimer = new Timer((int) (scrollTimeInSeconds * 1000 / totalSteps), new ActionListener() {
+
+        // Assign scroll to bottom timer, with required interval time
+        scrollBottomTimer = new Timer((int) (scrollTimeInSeconds * 1000 / totalSteps), new ActionListener() {
+            // Relevant instance variables; current step number, start scroll value
             private int currentStep = 0;
             private int startValue;
 
+            // Implement code when action on scroll bar is performed
             @Override
             public void actionPerformed(ActionEvent e) {
-                // System.out.println(currentStep + "-" + totalSteps);
-
+                // Get the vertical scroll bar and maximum of it
                 JScrollBar verticalScrollBar = getVerticalScrollBar();
                 int maximumValue = verticalScrollBar.getMaximum();
 
+                // If step value 0
                 if (currentStep == 0) {
+                    // Get start value; current scroll value
                     startValue = verticalScrollBar.getValue();
                 }
 
+                // Calculate next target value given step number
                 int targetValue = Math.min(startValue + (maximumValue - startValue) * currentStep / totalSteps, maximumValue);
+                // Scroll scrollbar to this value
                 verticalScrollBar.setValue(targetValue);
 
+                // If end of steps reached
                 if (currentStep++ > totalSteps) {
+                    // Reset step count
                     currentStep = 0;
-                    scrollTimer.stop();
+                    // Stop timer
+                    scrollBottomTimer.stop();
                 }
             }
         });
     }
 
-    private final Timer scrollTimer;
-    private final int totalSteps = 25; // Total steps to reach the bottom in 0.5 seconds
-
+    /**
+     * Smooth scroll to bottom method that calls to start scrollBottomTimer
+     */
     public void smoothScrollBottom() {
-        scrollTimer.start();
+        scrollBottomTimer.start();
     }
 
+    /**
+     * Smooth scroll method that calls to start smoothScrollTimer
+     *
+     * @param e MouseWheelEvent, used to get how much the scroll wheel has scrolled
+     */
     public void smoothScroll(MouseWheelEvent e) {
+        // Get what rotation
         int rotation = e.getWheelRotation();
+        // Get current scroll
         int currentY = getViewport().getViewPosition().y;
+        // Get maximum scroll to prevent glitching
         int maxScrollY = getVerticalScrollBar().getMaximum();
 
-        // System.out.println(maxScrollY - prevMax);
+        // If previous max is -1, unset
         if (prevMax == -1) {
+            // Set it to max scroll
             prevMax = maxScrollY;
-        } else if (Math.abs(maxScrollY - prevMax) > 100) {
-            // System.out.println(maxScrollY - prevMax);
+        } else if (Math.abs(maxScrollY - prevMax) > 100) { // Check if scroll value has changed too much
+            // If so, reset scroll value
             maxScrollY = prevMax;
         }
 
+        // Calculate destination
         destinationY = Math.max(0, Math.min(currentY + (rotation * multiplier * 3), maxScrollY));
-        timer.start();
+        // Start smooth scrolling
+        smoothScrollTimer.start();
     }
 
+    // Getters and setters
     public int getDestinationY() {
         return destinationY;
     }
@@ -122,12 +165,12 @@ public class JankScrollPane extends JScrollPane {
         this.destinationY = destinationY;
     }
 
-    public Timer getTimer() {
-        return timer;
+    public Timer getSmoothScrollTimer() {
+        return smoothScrollTimer;
     }
 
-    public void setTimer(Timer timer) {
-        this.timer = timer;
+    public void setSmoothScrollTimer(Timer smoothScrollTimer) {
+        this.smoothScrollTimer = smoothScrollTimer;
     }
 
     public int getMultiplier() {
@@ -136,5 +179,37 @@ public class JankScrollPane extends JScrollPane {
 
     public void setMultiplier(int multiplier) {
         this.multiplier = multiplier;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public int getPrevMax() {
+        return prevMax;
+    }
+
+    public void setPrevMax(int prevMax) {
+        this.prevMax = prevMax;
+    }
+
+    public Timer getScrollBottomTimer() {
+        return scrollBottomTimer;
+    }
+
+    public void setScrollBottomTimer(Timer scrollBottomTimer) {
+        this.scrollBottomTimer = scrollBottomTimer;
+    }
+
+    public int getTotalSteps() {
+        return totalSteps;
+    }
+
+    public void setTotalSteps(int totalSteps) {
+        this.totalSteps = totalSteps;
     }
 }
