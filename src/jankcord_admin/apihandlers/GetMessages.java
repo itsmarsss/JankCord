2d4139abc504744b3b66c7deaf2712ba24e44c1d
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-25 12:16:51 -0400

    Document GetMessages

--- src/jankcord_admin/apihandlers/GetMessages.java
+++ src/jankcord_admin/apihandlers/GetMessages.java
@@ -17,47 +17,64 @@ import java.util.Map;
 public class GetMessages implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        //System.out.println("Messages Requested");
-
+        // Check if user is authorized
         if (!JankcordAdmin.authorized(exchange)) {
+            // If not authorized, return 403 response code
             ServerCommunicator.sendResponse(exchange, "403");
+            // Return
             return;
         }
 
+        // Get header
         Map<String, List<String>> requestHeaders = exchange.getRequestHeaders();
 
         String username = requestHeaders.get("username").get(0);
         String otherID = requestHeaders.get("otherID").get(0);
 
+        // Get current and other User object
         FullUser current = null;
         FullUser other = null;
 
+        // Loop through all user
         for (FullUser account : AdminDataBase.getAccounts()) {
+            // If username matches
             if (account.getUsername().equals(username)) {
+                // Update current User
                 current = account;
             }
 
+            // If id matches
             if (String.valueOf(account.getId()).equals(otherID)) {
+                // Update other User
                 other = account;
             }
         }
 
+        // If current user is non-existent
         if (current == null) {
+            // If not authorized, return 403 response code
             ServerCommunicator.sendResponse(exchange, "403");
+            // Return
             return;
         }
 
+        // If other user is non-existent
         if(other == null) {
+            // If not authorized, return 403 response code
             ServerCommunicator.sendResponse(exchange, "404");
+            // Return
             return;
         }
 
+        /// Get id of users
         long otherIDNum = Long.parseLong(otherID);
         long currentIDNum = current.getId();
 
+        // Get file name [<smaller id>-<larger id>.json]
         String fileName = Math.min(otherIDNum, currentIDNum) + "-" + Math.max(otherIDNum, currentIDNum);
 
-        String message = """
+        // Message template
+        final String MESSAGE = """
                 {
                     "id": %s,
                     "content": "%s",
@@ -65,22 +82,30 @@ public class GetMessages implements HttpHandler {
                 },
                 """;
 
+        // Messages builder
         StringBuilder messages = new StringBuilder();
 
+        // If chat not stored in database
         if (!AdminDataBase.getConversations().containsKey(fileName)) {
+            // Create File object
             File file = new File(AdminDataBase.getParent() + "/messages/" + fileName + ".json");
+
+            // If file exists
             if (file.exists()) {
+                // Read file
                 JankFileKit.readMessages(fileName + ".json");
-            } else {
+            } else { // Otherwise
                 JankFileKit.create(file);
             }
-        } else {
+        } else { // If stored
+            // Loop each message and append to list
             for (Message msg : AdminDataBase.getConversations().get(fileName)) {
-                messages.append(message.formatted(msg.getSenderID(), msg.getContent(), msg.getTimestamp()));
+                messages.append(MESSAGE.formatted(msg.getSenderID(), msg.getContent(), msg.getTimestamp()));
             }
         }
 
-        String textJSON = """
+        // Main messages JSON string and format it with friends builder
+        String dmMessages = """
                 {
                     "users": [%s, %s],
                     "messages": [
@@ -89,12 +114,16 @@ public class GetMessages implements HttpHandler {
                 }
                 """.formatted(currentIDNum, otherIDNum, messages.toString());
 
-        JankFileKit.writeFile(AdminDataBase.getParent() + "/messages/" + fileName + ".json", textJSON);
+        // Write messages
+        JankFileKit.writeFile(AdminDataBase.getParent() + "/messages/" + fileName + ".json", dmMessages);
 
+        // Ud database doesn't contain this chat
         if (!AdminDataBase.getConversations().containsKey(fileName)) {
+            // Read it
             JankFileKit.readMessages(fileName + ".json");
         }
 
-        ServerCommunicator.sendResponse(exchange, textJSON);
+        // Return dm messages to client
+        ServerCommunicator.sendResponse(exchange, dmMessages);
     }
 }
